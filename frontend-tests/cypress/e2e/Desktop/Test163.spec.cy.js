6a7bedcea75174900806ca206f4ecaaeff2bf0ba
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-05-04 07:44:43 -0230

    Refactor test 163 to test both languages

--- frontend-tests/cypress/e2e/Desktop/Test163.spec.cy.js
+++ frontend-tests/cypress/e2e/Desktop/Test163.spec.cy.js
@@ -1,35 +1,22 @@
-import { utility } from "../../support/Utility"
-
 describe('test id 163 - verify Learn overview - Explore our Content - Manage Money in retirement', () => {
-    beforeEach(() => {
-        cy.visitAndWait('/en/learn')
-    })
-    it('Learn overview - Manage Money in retirement is visible', () => {
-        cy.get('h2').eq(2)
-            .should('be.visible')
+  ['en', 'fr'].forEach(lang => {
+    it(`[${lang}] Learn overview - Manage Money in retirement is visible`, () => {
+      cy.visit(`/${lang}/learn`).get('#mainContent h2').first().should('be.visible')
     })
 
-    it('Learn overview - card 1 is visible', () => {
-        cy.get('#section-0-card-0')
-            .should('be.visible')
-    })
+    it(`[${lang}] Learn overview - Manage money cards are visible and link to expected pages`, () => {
+      const cardSpecs = [
+        // TODO :: GjB :: using `aria-describedby` as a selector is bad; ask developers to add data-cy attributes
+        { 'aria-describedby': 'section-0-card-0', 'href': `/${lang}/learn/retirement-income-sources` },
+        { 'aria-describedby': 'section-0-card-1', 'href': `/${lang}/learn/planning-to-save-for-retirement` },
+      ]
 
-    it('Learn overview - card 2 is visible', () => {
-        cy.get('#section-1-card-1')
-            .should('be.visible')
+      cardSpecs.forEach(cardSpec => {
+        cy.visit(`/${lang}/learn`)
+          .get(`#mainContent a[aria-describedby=${cardSpec['aria-describedby']}]`)
+          .should('have.attr', 'href', cardSpec['href'])
+          .should('be.visible')
+      })
     })
-
-    it('Learn overview - card 1 link click url redirects to /en/fr/ when accessing /en/fr', () => {
-        let language = new utility().getLanguageTabletOrMonitorScreen()
-        cy.wait(2000)
-        cy.get('#section-0-card-0').click()
-        cy.location('pathname').should('equal', language ? '/en/learn/retirement-income-sources' : '/fr/learn/retirement-income-sources')
-    })
-
-    it('Learn overview - card 2 link click url redirects to /en/fr/ when accessing /en/fr', () => {
-        let language = new utility().getLanguageTabletOrMonitorScreen()
-        cy.wait(2000)
-        cy.get('#section-0-card-1').click()
-        cy.location('pathname').should('equal', language ? '/en/learn/planning-to-save-for-retirement' : '/fr/learn/planning-to-save-for-retirement')
-    })
+  })
 })
